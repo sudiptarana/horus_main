a150a6f4e94e94ad1d285b4a3b727519ab11f781
sudiptarana <sudiptarana00@gmail.com>
2020-07-09 22:11:34 +0530

    Create db_healthcare.sql

---
+++ db_healthcare.sql
@@ -0,0 +1,208 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 09, 2020 at 06:40 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_healthcare`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `appId` int(3) NOT NULL,
+  `patientIc` bigint(12) NOT NULL,
+  `scheduleId` int(10) NOT NULL,
+  `appSymptom` varchar(100) NOT NULL,
+  `appComment` varchar(100) NOT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'process'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `icDoctor` bigint(12) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `doctorId` int(3) NOT NULL,
+  `doctorFirstName` varchar(50) NOT NULL,
+  `doctorLastName` varchar(50) NOT NULL,
+  `doctorAddress` varchar(100) NOT NULL,
+  `doctorPhone` varchar(15) NOT NULL,
+  `doctorEmail` varchar(20) NOT NULL,
+  `doctorDOB` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`icDoctor`, `password`, `doctorId`, `doctorFirstName`, `doctorLastName`, `doctorAddress`, `doctorPhone`, `doctorEmail`, `doctorDOB`) VALUES
+(123456789, '123', 123, 'Doctor', 'Roy', 'Kolkata', '7356775863', 'Roy@gmail.com', '1990-04-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctorschedule`
+--
+
+CREATE TABLE `doctorschedule` (
+  `scheduleId` int(11) NOT NULL,
+  `scheduleDate` date NOT NULL,
+  `scheduleDay` varchar(15) NOT NULL,
+  `startTime` time NOT NULL,
+  `endTime` time NOT NULL,
+  `bookAvail` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctorschedule`
+--
+
+INSERT INTO `doctorschedule` (`scheduleId`, `scheduleDate`, `scheduleDay`, `startTime`, `endTime`, `bookAvail`) VALUES
+(40, '2015-12-13', 'Sunday', '09:00:00', '10:00:00', 'notavail'),
+(47, '2020-06-18', 'Monday', '12:00:00', '23:05:00', 'notavail'),
+(52, '2020-06-04', 'Wednesday', '02:10:00', '03:15:00', 'available'),
+(54, '2020-06-12', 'Monday', '02:15:00', '03:20:00', 'available'),
+(55, '2020-06-19', 'Tuesday', '04:13:00', '03:05:00', 'available');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `icPatient` bigint(12) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `patientFirstName` varchar(20) NOT NULL,
+  `patientLastName` varchar(20) NOT NULL,
+  `patientMaritialStatus` varchar(10) NOT NULL,
+  `patientDOB` date NOT NULL,
+  `patientGender` varchar(10) NOT NULL,
+  `patientAddress` varchar(100) NOT NULL,
+  `patientPhone` varchar(15) NOT NULL,
+  `patientEmail` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`icPatient`, `password`, `patientFirstName`, `patientLastName`, `patientMaritialStatus`, `patientDOB`, `patientGender`, `patientAddress`, `patientPhone`, `patientEmail`) VALUES
+(534, '123', 'Gopal', 'Aj', '', '1999-11-17', 'male', '', '', 'sudiptarana00@gmail.com'),
+(536, 'dolui123', 'Mr', 'Roy', '', '1990-12-17', 'male', '', '', 'dului@gmail.com'),
+(555, 'khan123', 'imran', 'khan', '', '2000-11-16', 'male', '', '', 'imran@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `pay_id` int(100) NOT NULL,
+  `user_id` int(200) NOT NULL,
+  `product_id` int(200) NOT NULL,
+  `payment_id` varchar(1000) NOT NULL,
+  `amount` varchar(100) NOT NULL,
+  `contact_number` varchar(200) NOT NULL,
+  `contact_email` varchar(300) NOT NULL,
+  `created_date` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`appId`),
+  ADD UNIQUE KEY `scheduleId_2` (`scheduleId`),
+  ADD KEY `patientIc` (`patientIc`),
+  ADD KEY `scheduleId` (`scheduleId`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`icDoctor`);
+
+--
+-- Indexes for table `doctorschedule`
+--
+ALTER TABLE `doctorschedule`
+  ADD PRIMARY KEY (`scheduleId`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`icPatient`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`pay_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `appId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+
+--
+-- AUTO_INCREMENT for table `doctorschedule`
+--
+ALTER TABLE `doctorschedule`
+  MODIFY `scheduleId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `pay_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_4` FOREIGN KEY (`patientIc`) REFERENCES `patient` (`icPatient`),
+  ADD CONSTRAINT `appointment_ibfk_5` FOREIGN KEY (`scheduleId`) REFERENCES `doctorschedule` (`scheduleId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
